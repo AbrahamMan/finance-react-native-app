cfda8a3642b5d726d947351bc1bfbd01b0a036f3
ridhwan <muhammadridhwandaud@gmail.com>
2018-01-09 16:00:38 +0800

    + first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
+
+class App extends Component{
+
+  render(){
+
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return(
+      <Provider store={store}>
+        <View>
+        	<Text>
+        		HELLO WORLD
+        	</Text>	
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
