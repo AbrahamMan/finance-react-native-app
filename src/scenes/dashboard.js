7de3a1b21a69477a58ba6db1a72ed59eb8f7e0a7
ridhwan <muhammadridhwandaud@gmail.com>
2018-01-16 16:46:06 +0800

    + add carousel

--- src/scenes/dashboard.js
+++ src/scenes/dashboard.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, WebView, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Header } from '../components/layouts';
+import Carousel from 'react-native-snap-carousel';
+import { sliderWidth, itemWidth } from './styles';
+import { ENTRIES } from './example/';
+import SliderEntry from '../components/carousel';
 
 class Dashboard extends Component {
 
@@ -11,17 +15,37 @@ class Dashboard extends Component {
 	    tabBarIcon: () => <Icon size={24} name="dashboard" color="white" />
 	}
 
+	_renderItem ({item, index}) {
+        return (
+            <SliderEntry
+              data={item}
+              even={(index + 1) % 2 === 0}
+            />
+        );
+    }
+
 	render() {
 		const { params } = this.props.navigation.state;
+		let paynowSrc = { uri: 'http://ridhwandaud.com' }
 		return (
-			<View>
-				<Header headerText="Dashboard" />
-				<Text>
-					Dashboard : 
-				</Text>	
-			</View>
+			<View style={styles.container}>
+				<Carousel
+	              ref={(c) => { this._carousel = c; }}
+	              data={ENTRIES}
+	              renderItem={this._renderItem}
+	              sliderWidth={sliderWidth}
+	              itemWidth={itemWidth}
+	            />
+            </View>
 		);
 	}
 }
+
+const styles = {
+	container: {
+        flex: 1,
+        paddingTop: 20,
+    },
+};
 
 export default Dashboard;
